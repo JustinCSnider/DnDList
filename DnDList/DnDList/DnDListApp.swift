7a3252363d3f6878267221f57968899e84ba15f9
Justin Snider <Jcsnider100@gmail.com>
2025-03-21 14:39:14 -0600

    Added Initial Project

---
+++ DnDList/DnDList/DnDListApp.swift
@@ -0,0 +1,17 @@
+//
+//  DnDListApp.swift
+//  DnDList
+//
+//  Created by Justin Snider on 3/21/25.
+//
+
+import SwiftUI
+
+@main
+struct DnDListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DnDList/DnDListTests/DnDListTests.swift
@@ -0,0 +1,16 @@
+//
+//  DnDListTests.swift
+//  DnDListTests
+//
+//  Created by Justin Snider on 3/21/25.
+//
+
+import Testing
+
+struct DnDListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
